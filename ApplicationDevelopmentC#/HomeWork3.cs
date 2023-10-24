7e487ff1e68350d061689dce8732c2f40695be02
Artyom <artyom_kudryavcev_91@mail.ru>
2023-10-24 23:13:35 +0700

    Добавьте файлы проекта.

---
+++ ApplicationDevelopmentC#/HomeWork3.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDevelopmentC_
+{
+    public class HomeWork3
+    {
+
+        public static int HasExit(int startI, int startJ, int[,] l)
+        {
+            int сountOutputs = 0;
+            int flag = 0;
+
+            if (l[startI, startJ] == 1) 
+            {
+                Console.WriteLine("Начальная точка находится в стене!");
+            }
+
+            else if(l[startI, startJ] == 2)
+            {
+                Console.WriteLine("Выход находится на входе!");
+            }
+                
+            var stack = new Stack<Tuple<int, int>>();
+
+            stack.Push(new (startI, startJ));
+
+            while(stack.Count > 0)
+            {
+                var temp = stack.Pop();
+
+                if ((temp.Item1 == 0 || temp.Item1 == l.GetLength(0)-1 || temp.Item2 == 0 || temp.Item2 == l.GetLength(1) - 1) && flag != 0)
+                {
+                    
+                    сountOutputs++;
+                }
+
+                l[temp.Item1, temp.Item2] = 1;
+
+                if(temp.Item2 > 0 && l[temp.Item1, temp.Item2-1] != 1)
+                {
+                    stack.Push(new(temp.Item1, temp.Item2 - 1)); //вверх
+                }
+
+                if (temp.Item2 +1 < l.GetLength(1) && l[temp.Item1, temp.Item2 + 1] != 1)
+                {
+                    stack.Push(new(temp.Item1, temp.Item2 + 1)); //вниз
+                }
+
+                if (temp.Item1 > 0 && l[temp.Item1-1, temp.Item2] != 1)
+                {
+                    stack.Push(new(temp.Item1-1, temp.Item2)); //влево
+                }
+
+                if (temp.Item1 + 1 < l.GetLength(0) && l[temp.Item1+1, temp.Item2] != 1)
+                {
+                    stack.Push(new(temp.Item1+1, temp.Item2)); //вправо
+                }
+                flag++;
+            }
+            return сountOutputs;
+        }
+
+    }
+}
---
+++ ApplicationDevelopmentC#/Program.cs
@@ -0,0 +1,39 @@
+﻿using ApplicationDevelopmentC_;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+
+        //HomeWork3
+
+        int[,] labirynth1 = new int[,]
+            {
+                {1, 1, 1, 1, 1, 1, 1 },
+                {1, 0, 0, 0, 0, 0, 1 },
+                {1, 0, 1, 1, 1, 0, 1 },
+                {0, 0, 0, 0, 1, 0, 0 },
+                {1, 1, 0, 0, 1, 1, 1 },
+                {1, 1, 1, 0, 1, 1, 1 },
+                {1, 1, 1, 0, 1, 1, 1 }
+            };
+        ;
+       
+        
+      
+        Console.WriteLine(HomeWork3.HasExit(0, 0, labirynth1));
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
