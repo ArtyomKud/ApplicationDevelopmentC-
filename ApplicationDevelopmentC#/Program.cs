15724730403dfe90da66e69c03be64081cca8c17
Artyom <artyom_kudryavcev_91@mail.ru>
2023-10-24 23:15:23 +0700

    Create project HomeWork3

--- ApplicationDevelopmentC#/Program.cs
+++ ApplicationDevelopmentC#/Program.cs
@@ -22,7 +22,7 @@ internal class Program
        
         
       
-        Console.WriteLine(HomeWork3.HasExit(0, 0, labirynth1));
+        Console.WriteLine(HomeWork3.HasExit(1, 1, labirynth1));
 
 
 
