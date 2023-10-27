7f7f67c3fdc4fc301677d831810ed8a8d8ef3984
Artyom <artyom_kudryavcev_91@mail.ru>
2023-10-27 22:45:40 +0700

    Добавил проверку переменной i

--- ApplicationDevelopmentC#/HomeWork4.cs
+++ ApplicationDevelopmentC#/HomeWork4.cs
@@ -13,13 +13,23 @@ namespace ApplicationDevelopmentC_
     {
 
         public int number = 15;
-        public List<int> ints = new List<int> {5, 66, 3, 4, 1, 10, 7, 8, 9, 6, 11, 88, 13, 14, 15 };
+        public List<int> ints = new List<int> {77, 66, 3, 4, 1, 10, 7, 8, 9, 6, 11, 88, 13, 14, 15 };
 
         public void HomeWork4Metod()
         {
             foreach(int i in ints)
             {
-                int x = number - i;
+                int x = 0;
+                if (i < number)
+                {
+                     x = number - i;
+
+                }
+                else
+                {
+                    continue;
+                }
+                
 
                 var list = ints.FirstOrDefault(m => ints.Contains(x - m) && m != i);
 
