c55fd2158fc92c639a82104e82c828fbd2e27e72
Artyom <artyom_kudryavcev_91@mail.ru>
2023-12-12 22:51:38 +0700

    Создал HomeWork9

---
+++ ApplicationDevelopmentC#/HomeWork9/HomeWork9.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ApplicationDevelopmentC_.HomeWork9
+{
+    public class HomeWork9
+    {
+
+        public XElement CreateXmlElement(JsonElement jsonElement, String str)
+
+        {
+
+            
+
+            XElement element = new XElement(str); 
+
+
+
+            switch (jsonElement.ValueKind)
+
+            {
+
+                case JsonValueKind.Object:
+
+                   
+
+                    foreach (JsonProperty property in jsonElement.EnumerateObject())
+
+                    {
+                        
+                        XElement propertyElement = CreateXmlElement(property.Value, property.Name);
+                       
+                        element.Add(propertyElement);
+                      
+
+
+                    }
+
+                    break;
+
+
+
+                case JsonValueKind.Array:
+
+                    
+
+                    foreach (JsonElement arrayElement in jsonElement.EnumerateArray())
+
+                    {
+                        
+                        XElement arrayItemElement = CreateXmlElement(arrayElement, arrayElement.ValueKind.ToString());
+                       
+                        element.Add(arrayItemElement);
+                        
+
+
+
+
+
+
+                       
+
+
+                    }
+
+                    break;
+
+
+
+                case JsonValueKind.String:
+                    element = new XElement(str, jsonElement.GetString());
+                    
+
+                   
+
+
+                    break;
+
+
+
+                case JsonValueKind.Number:
+
+                case JsonValueKind.True:
+
+                case JsonValueKind.False:
+
+                    element = new XElement(str, jsonElement.GetRawText());
+                    
+
+                    
+
+                    break;
+
+
+
+                case JsonValueKind.Null:
+
+                    element = new XElement("null", "true");
+
+                    break;
+
+                default:
+                    element = new XElement("null", "true");
+
+                    break;
+
+            }
+
+
+
+            return element;
+
+        }
+
+
+
+    }
+}
--- ApplicationDevelopmentC#/Program.cs
+++ ApplicationDevelopmentC#/Program.cs
@@ -2,11 +2,18 @@
 using ApplicationDevelopmentC_.HomeWork5;
 using ApplicationDevelopmentC_.HomeWork6;
 using ApplicationDevelopmentC_.HomeWork8;
+using ApplicationDevelopmentC_.HomeWork9;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using System.Xml;
+using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    public static void Main(string[] args)
     {
 
 
@@ -67,13 +74,56 @@ internal class Program
         // HomeWork8
 
 
-       HomeWork8 homeWork8 = new HomeWork8();
-        homeWork8.searchForFileByTypeAndText(args[0], args[1]);
+        /* HomeWork8 homeWork8 = new HomeWork8();
+          homeWork8.searchForFileByTypeAndText(args[0], args[1]);
+
+          */
+
+        //HomeWork9
 
 
 
+
+        HomeWork9 homeWork9 = new HomeWork9();
+
+        
+
+        string jsonString = "{\"name\": \"Федор\",\r\n\"age\": 22,\r\n\"brother\":[{\"name\":\"Петр\",\"age\": 23},\r\n{\"name\":\"Денис\",\"age\": 30}]\r\n  \r\n}";
+
+        using (JsonDocument document = JsonDocument.Parse(jsonString))
+
+        {
+
+            JsonElement jsonElement = document.RootElement;
+
+          
+
+            XElement rootElement = homeWork9.CreateXmlElement(jsonElement, jsonElement.ValueKind.ToString());
+
+
+
+            
+
+            XDocument xmlDocument = new XDocument();
+
+            xmlDocument.Add(rootElement);
+            
+
+
+
+           
+
+            xmlDocument.Save("output.xml");
+
+            
+
+        }
 
     }
+
+
+
+    
 
      /* private static void Calc_MyEventHandler1(object? sender, EventArgs e)
       {
